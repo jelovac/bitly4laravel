80b6bf8607d118d6748fa9fcf9ceff796cb3a8fe
Vladimir Jelovac <vladimir.jelovac@jeftinodosajta.com>
2013-12-27 17:03:12 +0100

    Initial commit

---
+++ src/Jelovac/Bitly4laravel/Bitly4laravelServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Jelovac\Bitly4laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class Bitly4laravelServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('jelovac/bitly4laravel');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['bitly4laravel'] = $this->app->share(function($app) {
+            $config = $app['config']->get('bitly4laravel::config');
+            return new Bitly4laravel($config);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('bitly4laravel');
+    }
+
+}
---
+++ src/Jelovac/Bitly4laravel/Facades/Bitly4laravel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jelovac\Bitly4laravel\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Bitly4laravel extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'bitly4laravel';
+    }
+
+}
