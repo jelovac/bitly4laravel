e99da620cc9059f48b8a44c5960508965443e0c0
Vladimir Jelovac <vladimir.jelovac@jeftinodosajta.com>
2013-12-31 02:14:54 +0100

    added configuration file

---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'apiKey' => 'your-api-key',
+    'login' => 'your-login',
+    // oAuth
+    'oauth_client_id' => 'your-client-id',
+    'oauth_client_secret' => 'your-client-secret',
+    'oauth_access_token' => 'your-access-token',
+    // Cache
+    'use_cache' => false,
+    'cache_expires' => 3600, // Default 3600 = 24 h - 1 Day
+);
