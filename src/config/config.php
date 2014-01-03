cd0843a7557256581b3ab3f8fbc8d4d1255eeba4
Vladimir Jelovac <vladimir.jelovac@jeftinodosajta.com>
2014-01-03 17:51:20 +0100

    Updated configuration file

--- src/config/config.php
+++ src/config/config.php
@@ -1,13 +1,26 @@
 <?php
 
+/**
+ * Configuraton file for bitly4laravel
+ * Populate only the needed fields and comment others
+ */
 return array(
-    'apiKey' => 'your-api-key',
-    'login' => 'your-login',
-    // oAuth
-    'oauth_client_id' => 'your-client-id',
-    'oauth_client_secret' => 'your-client-secret',
-    'oauth_access_token' => 'your-access-token',
-    // Cache
+    // Bitly username
+    'username' => 'your-username',
+    // Bitly password
+    'password' => 'your-password',
+    // Bitly Deprecated API key
+    'api_key' => 'your-api-key',
+    // Bitly Application ID
+    'client_id' => 'your-client-id',
+    // Bitly Application Secret
+    'client_secret' => 'your-client-secret',
+    // oAuth Generic Access Token
+    'access_token' => 'your-access-token',
+    // Cache enabled?
     'use_cache' => false,
+    // Cache Duration in minutes
     'cache_expires' => 3600, // Default 3600 = 24 h - 1 Day
+        // Unique cache key
+        //'cache_key' => ''
 );
