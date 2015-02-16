d809b122c19869f1459827938d04d28dca58c857
Vladimir Jelovac <jelovac@users.noreply.github.com>
2015-02-16 15:33:35 +0100

    Fixing config path

--- src/Jelovac/Bitly4laravel/ServiceProviders/Laravel5ServiceProvider.php
+++ src/Jelovac/Bitly4laravel/ServiceProviders/Laravel5ServiceProvider.php
@@ -11,7 +11,7 @@ class Laravel5ServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        $configPath = __DIR__ . '/../../config/bitly4laravel.php';
+        $configPath = __DIR__ . '/../../../config/bitly4laravel.php';
 
         $paths = array(
             $configPath => $this->config_path("bitly4laravel.php"),
@@ -27,7 +27,7 @@ class Laravel5ServiceProvider extends ServiceProvider {
      */
     public function register()
     {
-        $configPath = __DIR__ . '/../../config/bitly4laravel.php';
+        $configPath = __DIR__ . '/../../../config/bitly4laravel.php';
 
         $this->mergeConfigFrom($configPath, 'bitly4laravel');
 
